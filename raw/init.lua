af7224daf791011a2f20deed1ea8cb871d1ea965
Putnam3145 <bpwhite@cableone.net>
2014-03-24 12:09:07 -0700

    Renamed onItemComplete function to something more universal.

--- raw/init.lua
+++ raw/init.lua
@@ -10,17 +10,18 @@ eventful=require('plugins.eventful')
 
 eventful.enableEvent(eventful.eventType['ITEM_CREATED'],5)
 
-eventful.onItemCreated.artifactFantasticP=function(item_id)
+eventful.onItemCreated.fantasticP=function(item_id)
 	local item=df.item.find(item_id)
 	--check if item is eligible for this particular function
-	if (not item.flags.artifact or not (item:isWeapon() or item:getEffectiveArmorLevel()<2)) then return false end
-	local matInfo
-	--check if blunt or not; if blunt, set matInfo (defined above) to the blunt artifact mat, else sharp/armor artifact mat
-	if isBlunt(item) then
-		matinfo=dfhack.matinfo.find('ARTIFACT_BLUNT_P')
-	else
-		matinfo=dfhack.matinfo.find('ARTIFACT_SHARP_ARMOR_P')
+	if item.flags.artifact and (item:isWeapon() or item:getEffectiveArmorLevel()<2)
+		local matInfo
+		--check if blunt or not; if blunt, set matInfo (defined above) to the blunt artifact mat, else sharp/armor artifact mat
+		if isBlunt(item) then
+			matinfo=dfhack.matinfo.find('ARTIFACT_BLUNT_P')
+		else
+			matinfo=dfhack.matinfo.find('ARTIFACT_SHARP_ARMOR_P')
+		end
+		item:setMaterial(matInfo.type)
+		item:setMaterialIndex(matinfo.index)
 	end
-	item:setMaterial(matInfo.type)
-	item:setMaterialIndex(matinfo.index)
 end
