e1f26df5de6cd0a8779bc23d08fc1670096909c4
Putnam3145 <bpwhite@cableone.net>
2014-03-25 10:48:24 -0700

    Added fortress defense and mod manager.

---
+++ hack/mods/fortress defense bonus/init.lua
@@ -0,0 +1,11 @@
+local G=_G
+local _ENV={}
+
+name="Fortress Defense Bonus"
+raws_list={"creature_fdbonus.txt","entity_fdbonus.txt"}
+		   
+author="darkflagrance"
+description=[[
+Adds 4 hostile civilizations similarly sized and equipped to dwarves.
+]]
+return _ENV
