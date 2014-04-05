39cb75a4b8fb7011f43946dddc332f3aba0ce64a
Putnam3145 <bpwhite@cableone.net>
2014-04-04 19:28:53 -0700

    Made advpotions an option for now.

---
+++ hack/mods/advpotions/init.lua
@@ -0,0 +1,13 @@
+local G=_G
+local _ENV={}
+
+loadnum=4
+name="Adv potions"
+raws_list={"plant_fake_potion_fantastic.txt","reaction_potion_fantastic_adv.txt"}
+		   
+author="Putnam"
+description=[[
+Adds a bunch of really cheaty (for now)
+potions to Adventure mode.
+]]
+return _ENV
