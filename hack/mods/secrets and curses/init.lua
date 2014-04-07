1745a0699ce18568a0ccd4e3b5f3e12ae61ceaa6
Putnam3145 <bpwhite@cableone.net>
2014-04-07 14:46:37 -0700

    Made it uninstall and install correctly by renaming a single file.

--- hack/mods/secrets and curses/init.lua
+++ hack/mods/secrets and curses/init.lua
@@ -6,24 +6,6 @@ name="Secrets/Curses"
 raws_list={"inorganic_putnamagic.txt","interaction_secret_putnamagic.txt","interaction_sphere_putnamagic.txt","reaction_adventure_putnamagic.txt"}
 author="Putnam"
 
-function fileExists(filename)
-	local file=io.open(filename,"rb")
-	if file==nil then
-		return
-	else
-		file:close()
-		return true
-	end
-end
-
-checkInstalled=function()
-	for k,v in pairs(raws_list) do
-		if fileExists(dfhack.getDFPath().."/raw/objects/"..v) then
-			return true,v
-		end
-	end
-end
-
 description=[[
 Adds a bunch of secrets/curses.
 Recommended! On by default.
