930f1ed08a1042ffba34ad4f8416ad7fdf8bc296
Putnam3145 <bpwhite@cableone.net>
2014-04-07 13:55:21 -0700

    Made secrets/curses an option.

---
+++ hack/mods/secrets and curses/init.lua
@@ -0,0 +1,13 @@
+local G=_G
+local _ENV={}
+
+loadnum=5
+name="Secrets/Curses"
+raws_list={"inorganic_putnamagic.txt","interaction_secret_putnamagic.txt","interaction_sphere_putnamagic.txt","reaction_adventure_putnamagic.txt"}
+installed=true
+author="Putnam"
+description=[[
+Adds a bunch of secrets/curses.
+Recommended! On by default.
+]]
+return _ENV
